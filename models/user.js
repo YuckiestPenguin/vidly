ae348ffe8b80b835f18786aa741376056b8efedf
Vico <vinicomelo@gmail.com>
2018-11-05 14:40:16 -0500

    user now exports method to generate authToken

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,17 +25,22 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  return token;
+}
+
 const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(3).max(50).required(),
-        email: Joi.string().min(3).max(255).required().email(),
-        password: Joi.string().min(3).max(1024).required()
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(3).max(255).required().email(),
+    password: Joi.string().min(3).max(1024).required()
   };
 
   return Joi.validate(user, schema);
 }
 
-exports.User = User; 
+exports.User = User;
 exports.validate = validateUser;
