311304c5d0f94019d5bd938abf8b8fd89aa38e17
Vico <vinicomelo@gmail.com>
2018-11-05 14:24:33 -0500

    Added token to user post route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 
 router.post('/', async (req, res) => {
@@ -17,7 +19,10 @@ router.post('/', async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+
+  const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+
+  res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router
