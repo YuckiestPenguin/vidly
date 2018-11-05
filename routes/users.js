b2aef0377323d3bfe8c87fd9090e35fb51bf5cbc
Vico <vinicomelo@gmail.com>
2018-11-05 11:26:44 -0500

    Added lodash to users route.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const { User, validate } = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 
 router.post('/', async (req, res) => {
@@ -11,10 +12,9 @@ router.post('/', async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.')
 
-  user = new User({ name: req.body.name, email: req.body.email, password: req.body.password });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
   await user.save();
-
-  res.send(user);
+  res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router
