3178092c1c222783236cd4bc7d804804cab062f3
Vico <vinicomelo@gmail.com>
2018-11-06 19:01:21 -0500

    created error middlware

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,4 @@
-const {Genre, validate} = require('../models/genre');
+const { Genre, validate } = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -6,22 +6,27 @@ const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
 router.get('/', async (req, res) => {
-  const genres = await Genre.find().sort('name');
-  res.send(genres);
+  try {
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+  } catch (error) {
+    res.status(500).send('Something failed.')
+  }
+
 });
 
 router.post('/', auth, async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
-  
+
   res.send(genre);
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
@@ -29,7 +34,7 @@ router.put('/:id', async (req, res) => {
   });
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+
   res.send(genre);
 });
 
