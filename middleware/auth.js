de0bf1824f81a42c8dee5aa995a82bebd3b4b4a8
vinicomelo <vinicomelo@gmail.com>
2018-11-13 23:51:22 -0500

    Messing aroung with auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Acces denied. No token provided.');
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send('Acces denied. No token provided.');
 
-  try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded;
-    next();
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = decoded;
+        next();
 
-  } catch (error) {
-    res.status(400).send('Invalid Token.');
-  }
+    } catch (error) {
+        res.status(400).send('Invalid Token.');
+    }
 
 }
--- routes/genres.js
+++ routes/genres.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     const genres = await Genre.find().sort('name');
     res.send(genres);
